b85f1edfb400f7d8482a32c2db487a735be02d32
Shahjalal Khan <shahjalalkhanrabbi@gmail.com>
2023-12-20 09:19:52 +0600

    updated string operation

--- day1/strings.py
+++ day1/strings.py
@@ -3,6 +3,7 @@ a = """Hello ,there is a man who havent noting and who
 have pure heart"""
 print(type(a))
 
+#slicing
 print(a[6:10])
 print(a[-10:-3])
 
@@ -18,3 +19,36 @@ if "mans" not in a:
 b='Hii man'
 print(b[:5])
 print(b[2:])
+
+x ="  HeLlo, friEnds "
+print(x.upper())
+print(x.lower())
+print(x.strip())
+print(x.replace("H","J"))
+print(x.split(","))
+X = "hnkj vfbdddddd"
+print(X.capitalize())
+
+#concate
+p = "Hello"
+q = "There"
+r = p+q
+print(r)
+#string format
+age = 23
+txt = "This is Shahjalal Khan {}" 
+print(txt.format(age))
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
+
+
+#Escape character
+esc = "This is me \"who\" havent nothing"
+print(esc)
+
+
+
